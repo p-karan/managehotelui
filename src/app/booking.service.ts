e6abc77d91f2fd697945f5a1287c10a51a1bf1ba
a-rushiya <ashish.rushiya@gmail.com>
2019-12-16 19:10:43 +0530

    Booking details added

--- src/app/booking.service.ts
+++ src/app/booking.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {Booking} from './booking';
-import {Hotel} from "./hotel";
 
 
 @Injectable({
