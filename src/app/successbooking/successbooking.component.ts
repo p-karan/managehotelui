e0338901457f74ae18034b87c15d943dbcf35bdf
p-karan <praja.karan@gmail.com>
2019-12-18 04:43:33 +0530

    penultimate updates to UI

---
+++ src/app/successbooking/successbooking.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-successbooking',
+  templateUrl: './successbooking.component.html',
+  styleUrls: ['./successbooking.component.css']
+})
+export class SuccessbookingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
